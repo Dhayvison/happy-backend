cec9ce13ce8446ee50d9732f133a253a3c8434a3
Dhayvison Braga <dhayvison.braga@hostweb.com.br>
2021-03-19 21:06:05 -0300

    refactor: config to connect database in production

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -1,3 +1,25 @@
-import { createConnection } from 'typeorm';
+import { createConnection, getConnectionOptions } from 'typeorm';
 
-createConnection();
+export default (async () => {
+  const defaultOptions = await getConnectionOptions();
+  const isProdEnv = process.env.NODE_ENV === 'production';
+
+  const database = await createConnection(
+    Object.assign(defaultOptions, {
+      ...(isProdEnv && {
+        database: './dist/database/database.sqlite',
+        entities: ['./dist/models/**.js'],
+        migrations: ['./dist/database/migrations/**.js'],
+        cli: {
+          migrationsDir: './dist/database/migrations',
+        },
+      }),
+    }),
+  );
+
+  if (isProdEnv) {
+    await database.runMigrations();
+  }
+
+  return database;
+})();
